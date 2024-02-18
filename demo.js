0062adaf1bc73e60ba5fc53dc878f1bad4e5451a
Pramodku12 <142924067+Pramodku12@users.noreply.github.com>
2024-02-18 21:29:05 +0530

    added portfolio files

---
+++ demo.js
@@ -0,0 +1,8 @@
+        
+var typed = new typed(".auto-type",{
+    strings : ["programmer", "designer","youtuber"],
+    typespeed : 150,
+    backSpeed : 150,
+    backDelay : 1000,
+    loop : true
+});
